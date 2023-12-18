f1ab030155e476088bf1f057894e070e50bcf5c3
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2023-12-18 07:49:41 +0700

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,13 +8,19 @@ const Header: React.FC = () => {
   return (
     <header className="px-12 lg:px-20 py-12 relative z-10">
       <div className="flex justify-between flex-row gap-6">
-        <Link className="text-white cursor-pointer" to="/">
-          <div className="font-bold text-3xl">DymNEI</div>
-          <div className="font-mono pl-10">dym9.xyz</div>
-        </Link>
+        <div className="flex items-center gap-5">
+          <img src="/images/logo-transparent-bright.png" width={65} />
+          <div>
+            <Link className="text-white cursor-pointer" to="/">
+              <div className="font-bold text-3xl">DymNEI</div>
+              <div className="font-mono pl-10">dym9.xyz</div>
+            </Link>
+          </div>
+        </div>
+
         <div className="flex items-center gap-2 lg:gap-8 flex-col lg:flex-row">
-          <a href="https://dymension.xyz" target="_blank" rel="noopener noreferrer" className="underline">
-            dymension.xyz
+          <a href="https://portal.dymension.xyz/rollapp/pierreneter_20-1" target="_blank" rel="noopener noreferrer" className="underline">
+            Portal
           </a>
           <Link to="rewards" className="underline cursor-pointer" spy={true} smooth={true} duration={500} offset={-50}>
             Rewards
