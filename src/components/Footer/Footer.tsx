f35b62b8da4a4f31968dcf0cad6be0996f710d01
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2023-12-18 07:31:45 +0700

    update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,28 +1,34 @@
 // Copyright 2021 - 2024 Transflox LLC. All rights reserved.
 
-import { HeartFill } from "@styled-icons/bootstrap";
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC = () => {
   return (
     <footer className="pb-6 pt-12 container mx-auto">
-      <div className="flex items-center justify-between flex-col lg:flex-row gap-6">
-        <div className="w-[380px]">
-          <a href="http://dym9.xyz" target="_blank" rel="noopener noreferrer" className="text-3xl font-mono">
-            dym9.xyz
+      <div className="grid grid-cols-3">
+        <div className="flex items-center gap-5">
+          <a href="https://dymension.xyz/" target="_blank" rel="noopener noreferrer">
+            <div className="text-sm mb-2">Based on</div>
+            <img src="/images/logo-dymension-bright.png" alt="Dym" width={200} />
           </a>
+          <img src="/images/lucky-badge.png" alt="Lucky" />
+        </div>
+        <div className="flex items-center justify-center">
+          <Link to="/" rel="noopener noreferrer" className="text-3xl font-mono">
+            dym9.xyz
+          </Link>
           <div>
-            <span className="">Touch me at</span>{" "}
+            {/* Developer by Pierre <br /> member of Lucky Research community */}
+            {/* <span className="">Touch me at</span>{" "}
             <a href="http://pierreneter.com" target="_blank" rel="noopener noreferrer" className="underline code">
               pierreneter@gmail.com
             </a>
-            <HeartFill size={26} color="#ff0000" />
+            <HeartFill size={26} color="#ff0000" /> */}
           </div>
         </div>
 
-        <div className="flex items-center gap-5">
-          <img src="/images/lucky-badge.png" alt="Transflox" />
-
+        <div className="flex items-center gap-5 justify-end">
           <a href="http://transflox.com" target="_blank" rel="noopener noreferrer">
             <img src="/images/transflox-badge.png" alt="Transflox" width={180} />
           </a>
