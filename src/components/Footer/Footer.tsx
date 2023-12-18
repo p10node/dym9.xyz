7796d0f673404dd78a176c26e2bf771f6c30e79f
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2023-12-18 07:53:59 +0700

    update header and footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,13 +6,18 @@ import { Link } from "react-router-dom";
 const Footer: React.FC = () => {
   return (
     <footer className="pb-6 pt-12 container mx-auto">
-      <div className="grid grid-cols-3">
-        <div className="flex items-center gap-5">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="flex items-top gap-6 justify-center lg:justify-normal">
           <a href="https://dymension.xyz/" target="_blank" rel="noopener noreferrer">
             <div className="text-sm mb-2">Based on</div>
-            <img src="/images/logo-dymension-bright.png" alt="Dym" width={200} />
+            <img src="/images/logo-dymension-bright.png" alt="Dym" width={250} />
           </a>
-          <img src="/images/lucky-badge.png" alt="Lucky" />
+          <div>
+            <div className="text-sm mb-2">Member of</div>
+            <div className="flex items-center gap-2">
+              <img src="/images/lucky-badge.png" alt="Lucky" width={45} /> <span className="text-lg">Lucky Community</span>
+            </div>
+          </div>
         </div>
         <div className="flex items-center justify-center">
           <Link to="/" rel="noopener noreferrer" className="text-3xl font-mono">
@@ -28,7 +33,7 @@ const Footer: React.FC = () => {
           </div>
         </div>
 
-        <div className="flex items-center gap-5 justify-end">
+        <div className="flex items-center gap-5 justify-center lg:justify-end">
           <a href="http://transflox.com" target="_blank" rel="noopener noreferrer">
             <img src="/images/transflox-badge.png" alt="Transflox" width={180} />
           </a>
