2c48d89d8427aae7fc67540b9f4b46991beff891
Long Nguyễn Giao Huỳnh <pierre@pierreneter.com>
2023-12-19 12:30:04 +0700

    add GA

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@
 import "@rainbow-me/rainbowkit/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import ReactGA from "react-ga4";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 import App from "./components/App/App";
 import "./styles/main.scss";
 
+ReactGA.initialize("G-PDQD471S2P");
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
